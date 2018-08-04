7709539d592324e12f9e159ffabbc9d0f36a72a8
Anushree1997 <40564820+Anushree1997@users.noreply.github.com>
2018-08-04 22:34:18 +0530

    basic calculations with input

---
+++ python/day2/datatypes.py
@@ -0,0 +1,19 @@
+i=4
+d=4.0
+s="abc"
+# Declare second integer, double, and String variables.
+j=int(input())
+e=float(input())
+t=input()
+# Read and save an integer, double, and String to your variables.
+addition= int(i)+int(j)
+addition_1 =float(d)+float(e)
+string = s+t
+
+# Print the sum of both integer variables on a new line.
+print(addition )
+# Print the sum of the double variables on a new line.
+print(addition_1 ) 
+# Concatenate and print the String variables on a new line
+print(string)
+# The 's' variable above should be printed first.
